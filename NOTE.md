9c4c2db54328863aa8b8ec1ef55ea083b0d34968
Nam.Phan <namph.tech@gmail.com>
2021-06-05 14:32:35 +0700

    🛠 Created video-upload function and run test.

--- NOTE.md
+++ NOTE.md
@@ -1,5 +1,6 @@
 Noted for this commit and during the tutorial:
-- Form the video unit using struct in the col contract.
-- Hash of the video is the string returned from the IPFS.
-- Truffle command:
-  + truffle console
+- msg is a Solidity's global variable.
+- Create an event and emit the event at the end of the function to notify if the work has done.
+- Method "require(true)" to make sure all variables are filled in before execute the rest of the function.
+- mapping is like define interface of an object in typescript.
+- Ending function with ";" is IMPORTANT in Solidity.
--- src/contracts/DVideo.sol
+++ src/contracts/DVideo.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 contract DVideo {
   uint public videoCount = 0;
   string public name = "DVideo";
-  //Create id=>struct mapping
+  mapping(uint => Video) public videos;
 
   struct Video {
     uint id;
@@ -13,6 +13,13 @@ contract DVideo {
     address author;
   }
 
+  event VideoUploaded(
+    uint id,
+    string hash,
+    string title,
+    address author
+  );
+
   //Create Event
 
 
@@ -21,17 +28,19 @@ contract DVideo {
 
   function uploadVideo(string memory _videoHash, string memory _title) public {
     // Make sure the video hash exists
-
+    require(bytes(_videoHash).length > 0);
     // Make sure video title exists
-
+    require(bytes(_title).length > 0);
     // Make sure uploader address exists
-
+    require(msg.sender != address(0));
 
     // Increment video id
+    videoCount++;
 
     // Add video to the contract
-
+    videos[videoCount] = Video(videoCount, _videoHash, _title, msg.sender);
+    
     // Trigger an event
-
+    emit VideoUploaded(videoCount, _videoHash, _title, msg.sender);
   }
 }
